cd8532b4abf77cdbeb81dce5aa7703abebde97d0
darc-e <darcy.grewal@gmail.com>
2015-06-23 15:19:25 -0400

    local reset

--- public/mysqltest.php
+++ public/mysqltest.php
@@ -1,7 +1,7 @@
 <?
 		echo "test script<br/>";
-        $connect=mysql_connect("localhost","darcyg","buddha4040sd") or die("Unable to Connect");
-        mysql_select_db("darcyg_sunset_main") or die("Could not open the db");
+        $connect=mysql_connect("localhost","sunset","XvLTXT#Rg!sH") or die("Unable to Connect");
+        mysql_select_db("sunset_main") or die("Could not open the db");
         $showtablequery="SHOW TABLES FROM dbname";
         $query_result=mysql_query($showtablequery);
         while($showtablerow = mysql_fetch_array($query_result))
